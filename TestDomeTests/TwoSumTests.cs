35faf224f47212c08555bcaff97f301632a9b45a
Bill Johnson <wejdev@gmail.com>
2025-03-25 15:38:56 -0700

    Added TwoSum

---
+++ TestDomeTests/TwoSumTests.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(TwoSum))]
+public class TwoSumTests
+{
+    [Theory]
+    [InlineData(new[] { 3, 1, 5, 7, 5, 9 }, 10)]
+    [InlineData(new[] { 2, 4, 6, 8 }, 14)]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9)]
+    [InlineData(new[] { 3, 2, 4 }, 6)]
+    [InlineData(new[] { 3, 3 }, 6)]
+    public void FindTwoSum_FindsValidPair(int[] numbers, int target)
+    {
+        // Arrange
+        var list = numbers.ToList();
+
+        // Act
+        var result = TwoSum.FindTwoSum(list, target);
+        var actual = list[result.Item1] + list[result.Item2];
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(target, actual);
+        Assert.NotEqual(result.Item1, result.Item2);
+    }
+
+    [Fact]
+    public void FindTwoSum_ReturnsNull_WhenNoPairExists()
+    {
+        // Arrange
+        var numbers = new List<int> { 1, 2, 3 };
+        const int target = 100;
+
+        // Act
+        var result = TwoSum.FindTwoSum(numbers, target);
+
+        // Assert
+        Assert.Null(result);
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TestDome;
+
+class TwoSum
+{
+    public static Tuple<int, int>? FindTwoSum(List<int> list, int sum)
+    {
+        var map = new Dictionary<int, int>();
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            var missingNumber = sum - list[i];
+            if (map.TryGetValue(missingNumber, out var index))
+            {
+                return Tuple.Create(index, i);
+            }
+
+            map[list[i]] = i;
+        }
+
+        return null;
+    }
+
+    public static void Main(string[] args)
+    {
+        var indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+        if (indices != null)
+        {
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}
