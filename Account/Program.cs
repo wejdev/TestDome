2a3b145aab4d5adad6fcc9b1299b5def1686033e
Bill Johnson <wejdev@gmail.com>
2025-03-25 12:15:08 -0700

    Finished Account

--- Account/Program.cs
+++ Account/Program.cs
@@ -2,19 +2,25 @@
 
 class Account
 {
+
     [Flags]
     public enum Access
     {
-        Delete,
-        Publish,
-        Submit,
-        Comment,
-        Modify
+        Delete   = 1 << 0,
+        Publish  = 1 << 1,
+        Submit   = 1 << 2,
+        Comment  = 1 << 3,
+        Modify   = 1 << 4,
+
+        Writer = Submit | Modify,
+        Editor = Delete | Publish | Comment,
+        Owner  = Writer | Editor
     }
+
 
     public static void Main(string[] args)
     {
-        //Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
+        Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
     }
 }
 
@@ -22,7 +28,7 @@ class Account
 
 Each account on a website has a set of access flags that represent a users access.
 Update and extend the enum so that it contains three new access flags:
-	* A Writer access&nbsp;flag that is made up of the Submit and Modify flags.
+	* A Writer access flag that is made up of the Submit and Modify flags.
 	* An Editor access flag that is made up of the Delete, Publish and Comment flags.
 	* An Owner access that is made up of the Writer and Editor flags.
 
---
+++ TestDomeTests/AccountTests.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using Access = Account.Account.Access;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(Account.Account))]
+public class AccountTests
+{
+    [Fact]
+    public void WriterTest()
+    {
+        Assert.False(Access.Writer.HasFlag(Access.Delete));
+        Assert.False(Access.Writer.HasFlag(Access.Publish));
+        Assert.True(Access.Writer.HasFlag(Access.Submit));
+        Assert.False(Access.Writer.HasFlag(Access.Comment));
+        Assert.True(Access.Writer.HasFlag(Access.Modify));
+    }
+
+    [Fact]
+    public void EditorTest()
+    {
+        Assert.True(Access.Editor.HasFlag(Access.Delete));
+        Assert.True(Access.Editor.HasFlag(Access.Publish));
+        Assert.False(Access.Editor.HasFlag(Access.Submit));
+        Assert.True(Access.Editor.HasFlag(Access.Comment));
+        Assert.False(Access.Editor.HasFlag(Access.Modify));
+    }
+
+    [Fact]
+    public void OwnerTest()
+    {
+        Assert.True(Access.Owner.HasFlag(Access.Delete));
+        Assert.True(Access.Owner.HasFlag(Access.Publish));
+        Assert.True(Access.Owner.HasFlag(Access.Submit));
+        Assert.True(Access.Owner.HasFlag(Access.Comment));
+        Assert.True(Access.Owner.HasFlag(Access.Modify));
+    }
+}
