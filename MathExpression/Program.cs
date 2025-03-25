b7835053ae66ff92cc585ab5c0b85d9a844c3e59
Bill Johnson <wejdev@gmail.com>
2025-03-24 19:22:27 -0700

    Added MathExpression

---
+++ MathExpression/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace GamePlatform;
+
+internal class MathExpression
+{
+    public static bool IsBalancedOrig(string parenthesis)
+    {
+        Stack<char> stack = new Stack<char>();
+        HashSet<char> opening = new HashSet<char> { '(', '[', '{' };
+        HashSet<char> closing = new HashSet<char> { ')', ']', '}' };
+        Dictionary<char, char> pairs = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        foreach (char c in parenthesis)
+        {
+            if (opening.Contains(c))
+            {
+                stack.Push(c);
+            }
+            else if (closing.Contains(c))
+            {
+                if (stack.Count == 0 || stack.Peek() != pairs[c])
+                {
+                    return false;
+                }
+
+                stack.Pop();
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    public static bool IsBalanced(string input)
+    {
+        var stack = new Stack<char>();
+        var pairs = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        foreach (char c in input)
+        {
+            if (pairs.Values.Contains(c))
+            {
+                stack.Push(c);
+            }
+            else if (pairs.TryGetValue(c, out char expectedOpen))
+            {
+                if (stack.Count == 0 || stack.Pop() != expectedOpen)
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TestDomeTests/MathExpressionTests.cs
@@ -0,0 +1,31 @@
+﻿using GamePlatform;
+using JetBrains.Annotations;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(MathExpression))]
+public class MathExpressionTests
+{
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("()", true)]
+    [InlineData("{[123]sdaf}", true)]
+    [InlineData("{[123}sdaf}", false)]
+    [InlineData("1)23", false)]
+    [InlineData("1)(23", false)]
+    [InlineData("[3 + 5 x (4-1] -39]", false)]
+    [InlineData("()[]{}", true)]
+    [InlineData("([{}])", true)]
+    [InlineData("(]", false)]
+    [InlineData("([)]", false)]
+    [InlineData("(((())))", true)]
+    [InlineData("(((()))", false)]
+    public void IsBalancedTest(string parenthesis, bool expected)
+    {
+        // Act
+        var actual = MathExpression.IsBalanced(parenthesis);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
