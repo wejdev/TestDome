f614456983ee17679ff3cfa2af80bee802783735
Bill Johnson <wejdev@gmail.com>
2025-03-25 16:10:50 -0700

    Added Song

---
+++ Song/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace TestDome;
+
+public class Song
+{
+    private string _name;
+    public Song? NextSong { get; set; }
+
+    public Song(string name)
+    {
+        this._name = name;
+    }
+
+    public bool IsInRepeatingPlaylist()
+    {
+        var slow = this;
+        var fast = this;
+
+        while (fast?.NextSong != null)
+        {
+            slow = slow.NextSong;
+            fast = fast.NextSong.NextSong;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsInRepeatingPlaylistHashSet()
+    {
+        var visited = new HashSet<Song>();
+        var current = this;
+
+        while (current != null)
+        {
+            if (!visited.Add(current)) // Already seen this song
+                return true;
+
+            current = current.NextSong;
+        }
+
+        return false;
+    }
+
+    public static void Main(string[] args)
+    {
+        Song first = new Song("Hello");
+        Song second = new Song("Eye of the tiger");
+
+        first.NextSong = second;
+        second.NextSong = first;
+
+        Console.WriteLine(first.IsInRepeatingPlaylist());
+    }
+}
+
+/*
+
+A playlist is considered a repeating playlist if any of the songs contain a reference to a previous song in the playlist. Otherwise, the playlist will end with the last song which points to null.
+
+Implement a function `IsInRepeatingPlaylist` that, **efficiently** with respect to time used, returns true if a playlist is repeating or false if it is not.
+
+For example, the following code prints "True" as both songs point to each other.
+
+Song first = new Song("Hello");
+Song second = new Song("Eye of the tiger");
+
+first.NextSong = second;
+second.NextSong = first;
+
+Console.WriteLine(first.IsInRepeatingPlaylist());
+
+*/
---
+++ TestDomeTests/SongTests.cs
@@ -0,0 +1,74 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(Song))]
+public class SongTests
+{
+    [Fact]
+    public void IsInRepeatingPlaylistGivenTest()
+    {
+        var first = new Song("Hello");
+        var second = new Song("Eye of the tiger");
+
+        first.NextSong = second;
+        second.NextSong = first;
+
+        Assert.True(first.IsInRepeatingPlaylist());
+    }
+
+    [Fact]
+    public void IsInRepeatingPlaylistSample1Test()
+    {
+        var s1 = new Song("Track 1");
+        var s2 = new Song("Track 2");
+        var s3 = new Song("Track 3");
+        var s4 = new Song("Track 4");
+
+        s1.NextSong = s2;
+        s2.NextSong = s3;
+        s3.NextSong = s4;
+        s4.NextSong = s2; // cycle to s2
+
+        Assert.True(s1.IsInRepeatingPlaylist());
+    }
+
+    [Fact]
+    public void IsInRepeatingPlaylistSample2Test()
+    {
+        var a = new Song("A");
+        var b = new Song("B");
+        var c = new Song("C");
+
+        a.NextSong = b;
+        b.NextSong = c;
+        c.NextSong = a; // cycle back to "A"
+
+        Assert.True(a.IsInRepeatingPlaylist());
+    }
+
+    [Fact]
+    public void IsInRepeatingPlaylistSample3Test()
+    {
+        var x = new Song("X");
+        var y = new Song("Y");
+        var z = new Song("Z");
+
+        x.NextSong = y;
+        y.NextSong = z;
+        z.NextSong = null;
+
+        Assert.False(x.IsInRepeatingPlaylist());
+    }
+
+    [Fact]
+    public void IsInRepeatingPlaylistSample4Test()
+    {
+        var solo = new Song("Solo");
+
+        solo.NextSong = null;
+
+        Assert.False(solo.IsInRepeatingPlaylist());
+    }
+}
