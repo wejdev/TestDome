a01707d1b2bb8b8a57825f02cd5e642e04e6bb74
Bill Johnson <wejdev@gmail.com>
2025-03-25 20:54:52 -0700

    Added SortedSearch

---
+++ SortedSearch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TestDome;
+
+public class SortedSearch
+{
+    public static int CountNumbers(int[] sortedArray, int lessThan)
+    {
+        var left = 0;
+        var right = sortedArray.Length;
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (sortedArray[mid] < lessThan)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return left;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+}
+/*
+
+Implement function `CountNumbers` that accepts a sorted array of unique integers and,
+**efficiently** with respect to time used, counts the number of array elements that
+ are less than the parameter `lessThan`.
+
+For example, `SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4)` should return `2`
+because there are two array elements less than `4`.
+
+*/
---
+++ TestDomeTests/SortedSearchTests.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(SortedSearch))]
+public class SortedSearchTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, 10, 3)]
+    [InlineData(new int[] { 1, 3, 5, 7 }, 4, 2)]
+    [InlineData(new int[] { }, 5, 0)]
+    [InlineData(new int[] { 10, 20, 30 }, 15, 1)]
+    public void CountNumbersTest(int[] sortedArray, int lessThan, int expected)
+    {
+        var actual = SortedSearch.CountNumbers(sortedArray, lessThan);
+
+        Assert.Equal(expected, actual);
+    }
+}
