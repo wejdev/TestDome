00a7345e53ed5046e01345081a2372437ca8dc09
Bill Johnson <wejdev@gmail.com>
2025-03-25 19:10:49 -0700

    Added AlertService

---
+++ AlertService/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace TestDome;
+
+class AlertService
+{
+    public AlertService(IAlertDAO alertDAO)
+    {
+        _storage = alertDAO;
+    }
+
+    private readonly IAlertDAO _storage;
+
+    public Guid RaiseAlert()
+    {
+        return this._storage.AddAlert(DateTime.Now);
+    }
+
+    public DateTime GetAlertTime(Guid id)
+    {
+        return this._storage.GetAlert(id);
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public interface IAlertDAO
+{
+    Guid AddAlert(DateTime time);
+    DateTime GetAlert(Guid id);
+}
+
+public class AlertDAO : IAlertDAO
+{
+    private readonly Dictionary<Guid, DateTime> alerts = new Dictionary<Guid, DateTime>();
+
+    public Guid AddAlert(DateTime time)
+    {
+        Guid id = Guid.NewGuid();
+        this.alerts.Add(id, time);
+        return id;
+    }
+
+    public DateTime GetAlert(Guid id)
+    {
+        return this.alerts[id];
+    }
+}
+
+/*
+
+Refactor the **AlertService** and **AlertDAO** classes:
+
+- Create a new interface, named **IAlertDAO**, that contains the same methods as **AlertDAO**.
+- **AlertDAO** should implement the **IAlertDAO** interface.
+- **AlertService** should have a public constructor that accepts **IAlertDAO**.
+- The **RaiseAlert** and **GetAlertTime** methods should use the object passed through the constructor.
+
+*/
