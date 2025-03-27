c795d39f8188843fd0109ffdfe7fc2ec58143c56
Bill Johnson <wejdev@gmail.com>
2025-03-26 17:35:32 -0700

    Added TrainComposition

---
+++ TestDomeTests/TrainCompositionTests.cs
@@ -0,0 +1,116 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(TrainComposition))]
+public class TrainCompositionTests
+{
+    [Fact]
+    public void MainSampleTest()
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromLeft(7);
+        train.AttachWagonFromLeft(13);
+
+        int[] expected = [13, 7];
+        Assert.Equal(expected, train.Wagons);
+
+        expected = [13];
+        var fromRight = train.DetachWagonFromRight();
+        Assert.Equal(expected, train.Wagons);
+        Assert.Equal(7, fromRight);
+
+        expected = [];
+        var fromLeft = train.DetachWagonFromLeft();
+        Assert.Equal(expected, train.Wagons);
+        Assert.Equal(13, fromLeft);
+    }
+
+    [Fact]
+    public void AttachWagonFromLeftTest()
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromLeft(7);
+
+        int[] expected = [7];
+
+        Assert.Equal(expected, train.Wagons);
+
+        train.AttachWagonFromLeft(6);
+        train.AttachWagonFromLeft(5);
+
+        expected = [5, 6, 7];
+
+        Assert.Equal(expected, train.Wagons);
+    }
+
+    [Fact]
+    public void AttachWagonFromRightTest()
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromRight(7);
+
+        int[] expected = [7];
+
+        Assert.Equal(expected, train.Wagons);
+
+        train.AttachWagonFromRight(8);
+        train.AttachWagonFromRight(9);
+
+        expected = [7, 8, 9];
+
+        Assert.Equal(expected, train.Wagons);
+    }
+
+    [Fact]
+    public void DetachWagonFromLeftTest()
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromLeft(7);
+        train.AttachWagonFromLeft(6);
+        train.AttachWagonFromLeft(5);
+
+        int[] expected = [5, 6, 7];
+        Assert.Equal(expected, train.Wagons);
+
+        var fromLeft = train.DetachWagonFromLeft();
+        Assert.Equal(5, fromLeft);
+        expected = [6, 7];
+        Assert.Equal(expected, train.Wagons);
+    }
+
+    [Fact]
+    public void DetachWagonFromRight()
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromRight(7);
+        train.AttachWagonFromRight(8);
+        train.AttachWagonFromRight(9);
+
+        int[] expected = [7, 8, 9];
+        Assert.Equal(expected, train.Wagons);
+
+        var fromRight = train.DetachWagonFromRight();
+        Assert.Equal(9, fromRight);
+        expected = [7, 8];
+        Assert.Equal(expected, train.Wagons);
+    }
+
+
+    [Fact]
+    public void DetatchLeft_Throws_WhenListIsEmpty()
+    {
+        var train = new TrainComposition();
+
+        Assert.Throws<InvalidOperationException>(() => train.DetachWagonFromLeft());
+    }
+
+    [Fact]
+    public void DetatchRight_Throws_WhenListIsEmpty()
+    {
+        var train = new TrainComposition();
+
+        Assert.Throws<InvalidOperationException>(() => train.DetachWagonFromRight());
+    }
+}
---
+++ TrainComposition/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TestDome;
+
+public class TrainComposition
+{
+    public LinkedList<int> Wagons { get; } = new();
+
+    public void AttachWagonFromLeft(int wagonId)
+    {
+        Wagons.AddFirst(wagonId);
+    }
+
+    public void AttachWagonFromRight(int wagonId)
+    {
+        Wagons.AddLast(wagonId);
+    }
+
+    public int DetachWagonFromLeft()
+    {
+        if (Wagons.First == null)
+            throw new InvalidOperationException("No wagons to detach.");
+
+        var firstValue = Wagons.First.Value;
+        Wagons.RemoveFirst();
+        return firstValue;
+    }
+
+    public int DetachWagonFromRight()
+    {
+        if (Wagons.Last == null)
+            throw new InvalidOperationException("No wagons to detach.");
+
+        var lastValue = Wagons.Last.Value;
+        Wagons.RemoveLast();
+        return lastValue;
+    }
+
+    public static void Main(string[] args)
+    {
+        var train = new TrainComposition();
+        train.AttachWagonFromLeft(7);
+        train.AttachWagonFromLeft(13);
+        Console.WriteLine(train.DetachWagonFromRight()); // 7
+        Console.WriteLine(train.DetachWagonFromLeft()); // 13
+    }
+}
