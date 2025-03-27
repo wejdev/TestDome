574c14770fa20b7ef416359515f70d1cd972172c
Bill Johnson <wejdev@gmail.com>
2025-03-26 19:41:12 -0700

    Added Paragraph. Wasn't working because of the test having too many digits for one of the matches. Dang it.

---
+++ Paragraph/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TestDome;
+
+public class Paragraph
+{
+    public static string ChangeFormat(string paragraph)
+    {
+        const string regex = @"(\d{3})-(\d{2})-(\d{4})";
+        var changed = Regex.Replace(paragraph, regex,
+            match => match.Groups[1].Value + "/" + match.Groups[3].Value + "/" + match.Groups[2].Value);
+
+        return changed;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(ChangeFormat("Your long-term policy number is 112-39-8552."));
+    }
+}
---
+++ TestDomeTests/ParagraphTests.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(Paragraph))]
+public class ParagraphTests
+{
+
+    [Theory]
+    [InlineData("\"Policy: 112-39-8552 and 111-22-3333 completed\"", "\"Policy: 112/8552/39 and 111/3333/22 completed\"")]
+    [InlineData(@"Policy: 112-39-8552 and 111-22-3333 completed
+asdfasdf asdffsada", @"Policy: 112/8552/39 and 111/3333/22 completed
+asdfasdf asdffsada")]
+    [InlineData("Hello, World", "Hello, World")]
+    [InlineData("112-39-8552", "112/8552/39")]
+    [InlineData("\"Your long-term policy number is 112-39-8552.\"", "\"Your long-term policy number is 112/8552/39.\"")]
+    [InlineData("\"Policy: 112-39-8552\"", "\"Policy: 112/8552/39\"")]
+    [InlineData("\"112-39-8552 abc\"", "\"112/8552/39 abc\"")]
+    [InlineData("\"Policy: 112-39-8552 completed\"", "\"Policy: 112/8552/39 completed\"")]
+    public void ChangeFormat_ReturnsExpectedResult(string paragraph, string expected)
+    {
+        var result = Paragraph.ChangeFormat(paragraph);
+
+        Assert.Equal(expected, result);
+    }
+}
