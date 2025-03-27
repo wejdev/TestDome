0f2ab764444dfa0c53f03f78d30117abc49da486
Bill Johnson <wejdev@gmail.com>
2025-03-26 18:30:18 -0700

    Added MathUtils

---
+++ MathUtils/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestDome;
+
+public class MathUtils
+{
+    public static double Average(int a, int b)
+    {
+        return (a + b) / 2.0;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Average(2, 1));
+    }
+}
---
+++ TestDomeTests/MathUtilsTests.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using TestDome;
+
+namespace TestDomeTests;
+
+[TestSubject(typeof(MathUtils))]
+public class MathUtilsTests
+{
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(0, 2, 1)]
+    [InlineData(8, 4, 6)]
+    [InlineData(2, 1, 1.5)]
+    public void Average_ReturnsExpectedResult(int a, int b, double expected)
+    {
+        var actual = MathUtils.Average(a, b);
+
+        Assert.Equal(expected, actual);
+    }
+}
